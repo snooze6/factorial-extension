81bd77500a00053c1978c2daa13a5ccf00f1965f
Snooze <armandonogueirar@gmail.com>
2020-02-16 01:59:42 +0100

    I have a iFrame, not bad

--- ui/model/main.js
+++ ui/model/main.js
@@ -10,3 +10,34 @@ console.log('[+] - Extension loaded');
 let f = new factorial();
 
 console.log('[+] - Factorial core created');
+
+// Bypassing CSP
+function getHTML(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.overrideMimeType("text/html");
+    xhr.open("GET", browser.extension.getURL(url),false);
+    xhr.send(null);
+    if (xhr.status === 200)
+        return xhr.response;
+    else
+        throw new Error('File not found')
+}
+
+let html = getHTML("ui/test.html");
+// let html = getHTML("ui/compiled.html");
+console.log(html);
+
+let iframe = document.createElement("iframe");
+iframe.src = 'data:text/html;charset=utf-8,' + html;
+iframe.style.position = "fixed";
+iframe.style.bottom = 0;
+iframe.style.right = 0;
+iframe.style.width = "400px";
+iframe.style.height = "400px";
+iframe.style.overflow = "scroll";
+iframe.style.zIndex = 1337;
+iframe.style.minHeight = 0; // What a headache
+
+if (debug) iframe.style.backgroundColor = "cyan";
+
+document.body.appendChild(iframe);
